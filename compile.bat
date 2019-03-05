e0b25f7f9b2a0c86b20765a3f3cd4c8a0a87d7ee
Tiago Prata <prataaa@hotmail.com>
2019-03-05 20:48:00 -0300

    compile.bat deleta todos arquivos temporarios antes de compilar

--- compile.bat
+++ compile.bat
@@ -1,3 +1,27 @@
+echo Deleting files
+del "*.acn" /s /f /q
+del "*.acr" /s /f /q
+del "*.alg" /s /f /q
+del "*.aux" /s /f /q
+del "*.bbl" /s /f /q
+del "*.blg" /s /f /q
+del "*.brf" /s /f /q
+del "*.glg" /s /f /q
+del "*.glo" /s /f /q
+del "*.gls" /s /f /q
+del "*.glsdefs" /s /f /q
+del "*.idx" /s /f /q
+del "*.ilg" /s /f /q
+del "*.ind" /s /f /q
+del "*.ist" /s /f /q
+del "*.lof" /s /f /q
+del "*.log" /s /f /q
+del "*.loq" /s /f /q
+del "*.lot" /s /f /q
+del "*.sbl" /s /f /q
+del "*.simbolos" /s /f /q
+del "*.toc" /s /f /q
+
 pdflatex thesis.tex
 bibtex thesis
 makeindex thesis.idx
